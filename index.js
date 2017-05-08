c02caae8da00f2b051d9770215a56c3cf1a73bdf
Alexey Olexiuk <a.olexiuk@aimprosoft.com>
2017-05-08 16:55:14 +0300

    api server artists collection

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-/**
- * Created by Alexey on 08.05.2017.
- */
+var express = require('express');
+var bodyParser = require('body-parser');
+var ObjectID = require('mongodb').ObjectID;
+var db = require('./db');
+var artistsController = require('./controllers/artists');
+
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', function(req, res) {
+  res.send('Hello API')
+});
+
+app.get('/artists', artistsController.all);
+
+app.get('/artist/:id', artistsController.findById);
+
+app.post('/artists', artistsController.add);
+
+app.put('/artists/:id', artistsController.update);
+
+app.delete('/artists/:id', artistsController.delete);
+
+db.connect('mongodb://localhost:27017/myapi', function(err) {
+  if(err){
+    return console.log(err);
+  }
+  app.listen(3030, function() {
+    console.log("API Server Started!")
+  });
+});
